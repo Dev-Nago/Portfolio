a20f34d36265a3e33ea984de40b75394a942527d
unknown <jc13013@gmail.com>
2022-02-14 13:19:05 +0100

    added Socials

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import ContactForm from './Components/ContactForm';
+import Footer from './Components/Footer';
+import Infos from './Components/Infos';
+import NavBar from './Components/NavBar';
+import Projects from './Components/Projects';
+import Socials from './Components/Socials';
 
 function App() {
   return (
     <div className="App">
-      
+        <NavBar />
+        <Infos />
+        <Projects />
+        <Socials />
+        <ContactForm />
+        <Footer />
     </div>
   );
 }
---
+++ src/Components/Socials.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Socials() {
+  return (
+    <div className='text-5xl flex h-48 justify-center bg-black'>
+      <a href='https://github.com/Dev-Nago' target='blank_' rel="noreferrer">
+        <i className="fa-brands fa-github flex flex-row my-16 mx-5 text-white"></i>
+      </a>
+      <a href='https://linkedin.com/in/jean-claude-plaia-b2a454179' target='blank_' rel="noreferrer">
+        <i className="fa-brands fa-linkedin flex flex-row my-16 mx-5 text-white"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Socials
