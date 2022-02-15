a20b9557be44aa87a285ba27764bdd94d116371c
unknown <jc13013@gmail.com>
2022-02-16 00:39:54 +0100

    updated projects/technos

--- src/Components/Technologies.js
+++ src/Components/Technologies.js
@@ -27,9 +27,9 @@ function Technologies() {
   <div className='bg-black h-full p-10' id='Technos'>
     <div className='bg-white justify-center rounded-xl m-auto sm:w-1/2'>
         <h1 className='text-3xl font-bold'>Technologies</h1>
-        <div className='grid grid-cols-2 gap-2 text-2xl m-5 sm:grid-cols-3'>
+        <div className='grid grid-cols-2 gap-2 text-2xl m-5 lg:grid-cols-4'>
             {datas.map( (data, key) => (
-              <div key={key} className='m-5 border-2 border-orange-400 rounded-lg'>
+              <div key={key} className='m-3 border-2 border-orange-400 rounded-lg'>
                 <div>{data.name}</div>
                 <div><i className={data.icon}></i></div>
               </div>
