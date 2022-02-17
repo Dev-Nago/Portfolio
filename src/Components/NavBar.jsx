05cc91020dafeb20f34d28a13261359f121a50c3
unknown <jc13013@gmail.com>
2022-02-17 03:05:54 +0100

    minor update navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -14,8 +14,8 @@ function NavBar() {
     <div className='h-24 bg-black text-white flex flex-row'>
       <h1 className='font-bold text-2xl m-auto z-40'>Plaia Jean-Claude</h1>
       <i className={isOpen ? 'fa-solid fa-x text-2xl m-auto hover:cursor-pointer' : 'fa-solid fa-bars m-auto text-2xl hover:cursor-pointer'} onClick={toggle}></i>
-        <div className={isOpen ? 'z-40 h-screen w-screen bg-black fixed left-0 flex flex-col ease-in-out duration-500 sm:w-96' : 'h-screen w-screen bg-black fixed flex flex-col -left-full ease-in-out duration-500 sm:w-96'} onClick={toggle}>
-          <div className='m-auto h-1/2 w-1/2 flex flex-col space-y-12'>
+        <div className={isOpen ? 'z-40 h-full w-full bg-black fixed left-0 flex flex-col ease-in-out duration-500 sm:w-96' : 'h-screen w-screen bg-black fixed flex flex-col -left-full ease-in-out duration-500 sm:w-96'} onClick={toggle}>
+          <div className='m-5 flex flex-col space-y-8'>
             <i className='fa-solid fa-x text-2xl hover:cursor-pointer' onClick={toggle}></i>
             <a href='#Infos' className={NavbarItemStyle} onClick={toggle}>Infos</a>
             <a href='#Projects' className={NavbarItemStyle} onClick={toggle}>Projets</a>
