227741ed74447cd9b0b640a57174df468256d360
Plaia JC <32202647+Dev-Nago@users.noreply.github.com>
2024-07-08 13:38:17 +0200

    Update Infos.jsx

--- src/Components/Infos.jsx
+++ src/Components/Infos.jsx
@@ -5,16 +5,14 @@ function Infos() {
     <div className="h-screen text-black" id="Infos">
       <div className="h-screen flex flex-col justify-center">
         <h1 className="text-2xl font-semibold m-5">
-          Bienvenue sur mon Portfolio,
+          Bienvenue,
         </h1>
         <h2 className="font-mono text-lg">Je me présente Plaia Jean-Claude</h2>
         <span className="text-orange-400 text-2xl font-semibold italic m-5">
-          Développeur Web Junior
+          Développeur Web Junior / Data IA enthousiaste
         </span>
         <p className="m-5">
-          Actuellement à la recherche d'un Stage/Période d'essai pour parfaire
-          mes compétences en Front/Backend notamment React/Node et décrocher une
-          première expérience professionnelle enrichissante.
+          Actuellement à la recherche d'un Bootcamp/Formation avec Alternance dans le domaine de la DATA/IA Dev, vous pouvez retrouver ici quelques projets et techno utilisés durant ma formation dev.
         </p>
       </div>
     </div>
