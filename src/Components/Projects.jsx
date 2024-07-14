ef3c0d38ad0d5ab1f17477703b7e8651d41cc24d
Plaia JC <32202647+Dev-Nago@users.noreply.github.com>
2024-07-14 19:38:35 +0200

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,7 +6,9 @@ import image3 from "../Images/projet3.jpeg";
 function Projects({ isDarkMode }) {
   const projects = [
     {
-      image: {image1},
+      images: {
+        image : {image1}
+      },
       icon: "fa-solid fa-address-card text-5xl m-2",
       title: "Projet Portfolio",
       description: "Portfolio actuel réalisé dans le cadre de l'apprentissage de ReactJS",
@@ -18,7 +20,9 @@ function Projects({ isDarkMode }) {
       github: "https://github.com/Dev-Nago/portfolio",
     },
     {
-      image: {image2},
+      images: {
+        image : {image2}
+      },
       icon: "fa-brands fa-bitcoin text-5xl m-2",
       title: "Projet Crypto",
       description: "Mini Projet durant la formation réalisé en 2j sur un Thème donné.",
@@ -32,7 +36,9 @@ function Projects({ isDarkMode }) {
       github: "https://github.com/Dev-Nago",
     },
     {
-      image: {image3},
+      images: {
+        image : {image3}
+      },
       icon: "fa-solid fa-circle-question text-5xl m-2",
       title: "Projet Quiz",
       description: "Projet de Quiz réalisé durant mon stage de formation comprenant inscription/connexion, base de données, classement etc...",
@@ -63,7 +69,7 @@ function Projects({ isDarkMode }) {
           >
             <i className={project.icon}></i>
             <h2 className="m-2 text-xl font-semibold">{project.title}</h2>
-            <img src={project.image} class="m-2 rounded object-cover" alt="Image de preview du site" />
+            <img src={project.images.image} class="m-2 rounded object-cover" alt="Image de preview du site" />
             <p className="m-2">{project.description}</p>
             <p className="m-2">Technos :</p>
             <ul>
