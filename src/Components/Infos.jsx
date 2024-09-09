826fa0cecb63ae762faccce2e64ad98ba6b5935c
Plaia JC <32202647+Dev-Nago@users.noreply.github.com>
2024-09-09 13:35:17 +0200

    Update Infos.jsx

--- src/Components/Infos.jsx
+++ src/Components/Infos.jsx
@@ -14,7 +14,7 @@ function Infos() {
         <p className="m-5 max-w-5xl">
           Passionné d'informatique au sens large depuis tout jeune, j'ai suivi une formation de Développeur Web et Web Mobile en 2021 et j'ai également effectué une découverte des différents métiers du numérique pendant 1 mois et me suis pris de passion pour l'IA et la Data. Vous pouvez retrouver ici quelques projets et technologies utilisés durant ma formation dev web.
         </p>
-        <p className="font-mono text-lg m-5 max-w-5xl">Je suis actuellement en formation de développeur IA/Data chez Simplon.co et je recherche une Alternance en Développement IA/Data Analyst pour Mars 2025.</p>
+        <p className="font-mono text-lg m-5 max-w-5xl">Je suis actuellement en formation de développeur IA/Data avec Ecole IA MICROSOFT by Simplon.co et je recherche une Alternance dans le domaine pour mi Février 2025.</p>
       </div>
     </div>
   );
